aa3d5bba8e389c16b03500025e73c4d3b7e079fa
takashi-uchida <takashi.uchida.office@gmail.com>
2024-02-20 23:19:21 +0900

    for redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,6 +136,30 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: ['/'], // リダイレクト元のパス
+            to: '/docs/intro', // 目的地のパス
+          },
+        ],
+      },
+    ],
+    // [
+    //   '@docusaurus/plugin-ideal-image',
+    //   {
+    //     size: 500,
+    //     // quality: 70,
+    //     // max: 640, // max resized image's size.
+    //     // min: 640, // min resized image's size. if original is lower, use that size.
+    //     // steps: 1, // the max number of images generated between min and max (inclusive)
+    //     disableInDev: false,
+    //   },
+    // ],
+  ],
 };
 
 export default config;
